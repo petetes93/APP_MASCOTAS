c7d5162fde29bd90f1e664b22e0aab2c5a5bf6b0
petetes93 <alejandrolobatohaurie@gmail.com>
2023-12-18 00:48:50 +0100

    first commit client

---
+++ client/project/src/App.jsx
@@ -0,0 +1,39 @@
+import { colors } from '@mui/material'
+import './styles.css'
+
+function App() {
+  return (
+    <>
+      <html>
+        <div>
+          <h1>primeros pasos</h1>
+          <h3>vamos por buen camino</h3>
+          <ul>
+            <li style={{ backgroundColor: 'green' }}>Esto</li>
+            <li>Es</li>
+            <li>una</li>
+            <li>Prueba</li>
+          </ul>
+        </div>
+        <div>
+          <section>
+            <h1>PRUEBA 1</h1>
+            <p
+              style={{
+                backgroundColor: 'red',
+                textAlign: 'left',
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+              dolorem obcaecati delectus eius quas perferendis cum animi
+              nesciunt veniam perspiciatis blanditiis quisquam quos soluta
+              consectetur iste odio enim commodi asperiores.
+            </p>
+          </section>
+        </div>
+      </html>
+    </>
+  )
+}
+
+export default App
