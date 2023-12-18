b36d25cf394bdd7e454e3918604c487ede338500
petetes93 <alejandrolobatohaurie@gmail.com>
2023-12-18 23:40:59 +0100

    modificado la card del perro

--- client/project/src/pages/mascotaPage/Mascota.jsx
+++ client/project/src/pages/mascotaPage/Mascota.jsx
@@ -4,12 +4,10 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
 
 const Mascota = () => {
   const [pet, setPet] = useState(null)
-  const [userName, setUserName] = useState('John Doe')
+  const [userName, setUserName] = useState('Alejandro')
 
   useEffect(() => {
     fetch('http://localhost:3001/api/pets')
@@ -29,43 +27,52 @@ const Mascota = () => {
           marginLeft: 2,
           backgroundColor: '#c8e6c9',
           textAlign: 'center',
+          border: '2px solid #424242',
         }}
       >
         <CardContent>
           <Typography sx={{ fontSize: 30 }}>{userName}</Typography>
         </CardContent>
       </Card>
-      <Box
-        flexGrow={1}
-        display='flex'
-        justifyContent='center'
-        flexDirection='column'
-        alignItems='center'
-      >
-        {pet && (
-          <Card sx={{ width: 400, margin: 2 }}>
-            <CardContent>
-              <Typography sx={{ fontSize: 25 }} color='black' gutterBottom>
-                {pet.name}
-              </Typography>
-              <Typography variant='h5' component='div'>
-                {pet.breed}
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-                {pet.type}
-              </Typography>
-              <Typography variant='body2'>
-                {pet.description}
-                <br />
-                {'Buen perro'}
-              </Typography>
-            </CardContent>
-            <Button variant='contained' sx={{ margin: 2 }}>
-              Perfil
-            </Button>
-          </Card>
-        )}
-      </Box>
+      {pet && (
+        <Card
+          sx={{
+            width: 400,
+            margin: 2,
+            border: '2px solid #424242',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <CardContent
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              flexWrap: 'nowrap',
+            }}
+          >
+            <Typography sx={{ fontSize: 25 }} color='black' gutterBottom>
+              {pet.name}
+            </Typography>
+            <Typography variant='h5' component='div' sx={{ marginLeft: 2 }}>
+              {pet.breed}
+            </Typography>
+            <Typography sx={{ mb: 1.5, marginLeft: 2 }} color='text.secondary'>
+              {pet.type}
+            </Typography>
+            <Typography variant='body2' sx={{ marginLeft: 2 }}>
+              {pet.description}
+              <br />
+              {'Buen perro'}
+            </Typography>
+          </CardContent>
+          <Button variant='contained' sx={{ margin: 2 }}>
+            Perfil
+          </Button>
+        </Card>
+      )}
     </Box>
   )
 }
