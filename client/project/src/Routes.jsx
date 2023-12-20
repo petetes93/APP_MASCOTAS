6cb629aae62dc5c24612a414f2b5d225c88a3af9
petetes93 <alejandrolobatohaurie@gmail.com>
2023-12-20 12:51:57 +0100

    refactorizado el sistema de enroutador

--- client/project/src/Routes.jsx
+++ client/project/src/Routes.jsx
@@ -1,38 +1,74 @@
-// AppRoutes.jsx
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import LogIn from './pages/login/LogIn'
-import SignUp from './pages/register/SignUps'
-import Mascota from './pages/mascotaPage/Mascota'
-import PerfilMascota from './pages/perfilMascota/PerfilMascota'
-import CrearMascota from './pages/crearMascota/CrearMascota'
-import EditarPerfil from './pages/editarPerfil/EditarPerfil'
-import GestionClinica from './pages/GestionClinica/GestionClinica'
-import CrearMedicamento from './pages/CrearMedicamento/CrearMedicamento'
-import CrearVacuna from './pages/CrearVacuna/CrearVacuna'
+import {
+  createBrowserRouter,
+  RouterProvider as RouterProviderRRD,
+} from 'react-router-dom'
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route
-        path='/signup'
-        element={<SignUp onClose={() => {}} onRegister={() => {}} />}
-      />
-      <Route
-        path='/login'
-        element={<LogIn onLogin={() => {}} isAuthenticated={false} />}
-      />
-      <Route path='/mascotas' element={<Mascota />} />
-      <Route path='/perfil-mascota/:id' element={<PerfilMascota />} />
-      <Route path='/crear-mascota' element={<CrearMascota />} />
-      <Route path='/editar-perfil/:id' element={<EditarPerfil />} />
-      <Route path='/gestion-clinica/:id' element={<GestionClinica />} />
-      <Route path='/crear-medicamento/:id' element={<CrearMedicamento />} />
-      <Route path='/crear-vacuna/:id' element={<CrearVacuna />} />
-    </Routes>
-  )
-}
+import { RootLayout } from 'layouts'
 
-export default AppRoutes
+import {
+  Home,
+  ErrorPage,
+  SignUp,
+  LogIn,
+  Mascota,
+  PerfilMascota,
+  CrearMascota,
+  EditarPerfil,
+  GestionClinica,
+  CrearMedicamento,
+  CrearVacuna,
+} from 'pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/login',
+        element: <LogIn onLogin={() => {}} isAuthenticated={false} />,
+      },
+      {
+        path: '/mascotas',
+        element: <Mascota />,
+      },
+      {
+        path: '/perfil-mascota/:id',
+        element: <PerfilMascota />,
+      },
+      {
+        path: '/crear-mascota',
+        element: <CrearMascota />,
+      },
+      {
+        path: '/editar-perfil/:id',
+        element: <EditarPerfil />,
+      },
+      {
+        path: '/gestion-clinica/:id',
+        element: <GestionClinica />,
+      },
+      {
+        path: '/crear-medicamento/:id',
+        element: <CrearMedicamento />,
+      },
+      {
+        path: '/crear-vacuna/:id',
+        element: <CrearVacuna />,
+      },
+    ],
+  },
+])
+
+const RouterProvider = () => <RouterProviderRRD router={router} />
+
+export default RouterProvider
--- client/project/vite.config.js
+++ client/project/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      hooks: '/src/hooks',
+      pages: '/src/pages',
+      layouts: '/src/layouts',
+      services: '/src/services',
+    },
+  },
 })
